b4ddd1601d9ece5b7b7e4a2aafa43bbb83bc2cee
Arun Dhyani <dhyaniarun7@gmail.com>
2023-03-23 18:20:47 +0530

    subgraph config added for nft marketplace

---
+++ tests/nft-marketplace-utils.ts
@@ -0,0 +1,99 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  itemBought,
+  itemCanceled,
+  itemListed
+} from "../generated/NftMarketplace/NftMarketplace"
+
+export function createitemBoughtEvent(
+  buyer: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): itemBought {
+  let itemBoughtEvent = changetype<itemBought>(newMockEvent())
+
+  itemBoughtEvent.parameters = new Array()
+
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemBoughtEvent
+}
+
+export function createitemCanceledEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt
+): itemCanceled {
+  let itemCanceledEvent = changetype<itemCanceled>(newMockEvent())
+
+  itemCanceledEvent.parameters = new Array()
+
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return itemCanceledEvent
+}
+
+export function createitemListedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): itemListed {
+  let itemListedEvent = changetype<itemListed>(newMockEvent())
+
+  itemListedEvent.parameters = new Array()
+
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemListedEvent
+}
